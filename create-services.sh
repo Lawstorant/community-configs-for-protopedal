a06e20d4a8d8aefb5bda9befc64366c06a805230
Tomasz Pakuła <forest10pl@gmail.com>
2023-11-19 22:38:34 +0100

    Working service creation

--- create-services.sh
+++ create-services.sh
@@ -1,3 +1,45 @@
 #!/usr/bin/env bash
 
-echo "Services"
+echo ""
+echo "Creating systemd services for protopedal"
+echo ""
+
+# list device files in device-database
+DEVICES=$(echo device-database/*)
+SERVICES_DIR=services
+
+mkdir "$SERVICES_DIR"
+
+for DEVICE in $DEVICES; do
+    SERVICE_CONTENT=$(cat templates/service-template.service)
+    SERVICE_NAME="protopedal-<device>@.service"
+    NAME=""
+    SHORTNAME=""
+
+    echo "Creating service for $DEVICE"
+
+    while read -r LINE; do
+        PROPERTY=$(cut -d "=" -f 1 <<< "$LINE")
+        VALUE=$(cut -d "=" -f 2 <<< "$LINE")
+
+        case $PROPERTY in
+            name)
+                NAME="$VALUE"
+                ;;
+
+            shortname)
+                SHORTNAME="$VALUE"
+                ;;
+        esac
+    done < "$DEVICE"
+
+    SERVICE_CONTENT=$(sed "s/<device>/$NAME/g" <<< "$SERVICE_CONTENT")
+    SERVICE_CONTENT=$(sed "s/<device-short>/$SHORTNAME/g" <<< "$SERVICE_CONTENT")
+
+    SERVICE_NAME=$(sed "s/<device>/$SHORTNAME/g" <<< "$SERVICE_NAME")
+
+    echo "$SERVICE_CONTENT" > "$SERVICES_DIR/$SERVICE_NAME"
+done
+
+echo ""
+echo "Done creating systemd services for protopedal"
