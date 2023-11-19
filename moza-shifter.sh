9b524fbecca4a03ba8af1522311fef36d3170a55
Tomasz Pakuła <forest10pl@gmail.com>
2023-11-19 18:54:06 +0100

    Initial commit

---
+++ moza-shifter.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+protopedal \
+    --name "MOZA Racing HGP Shifter" \
+    -v 346e \
+    -p 001e \
+    --grab \
+    --no-auto-axes \
+    -a X -a Y -a Z \
+    --no-auto-buttons \
+    -b BASE -s PINKIE \
+    -b BASE2 -s BASE \
+    -b BASE3 -s BASE2 \
+    -b BASE4 -s BASE3 \
+    -b BASE5 -s BASE4 \
+    -b BASE6 -s BASE5 \
+    -b BASE7 -s BASE6 \
+    -b BASE8 -s TOP2 \
+    $1
